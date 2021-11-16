8522ac6ba1c03d9f9fdaa285c1400dcc5268d4dc
simonypkemeule <0323343@student.rocvantwente.nl>
2021-11-16 20:21:44 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tentamen_moa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tentamen_moa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+//corona.lmao.ninja/v2/countries
+
+import 'package:flutter/material.dart';
+
+//import 'package:flutter_tutorials/widgets/nav-drawer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavDrawer(),
+      appBar: AppBar(
+        title: Text('Side menu'),
+      ),
+      body: Center(
+        child: Text('Side Menu Tutorial'),
+      ),
+    );
+  }
+}
